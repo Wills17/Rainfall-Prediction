891232de2a7ecbec2fc692d59860ed5630d18e2c
Williams Odunayo <williamsodunayo70@gmail.com>
2025-01-28 20:53:38 +0100

    Add exploratory data analysis and handle missing values in Rainfall_prediction.py

--- Rainfall-Prediction/Rainfall_prediction.py
+++ Rainfall-Prediction/Rainfall_prediction.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
 # Load the dataset
 """Data Collection and Processing"""
@@ -22,3 +23,21 @@ data.columns = data.columns.str.strip()
 
 # Check for missing values in the dataset
 print(data.isnull().sum())
+
+print(data["winddirection"].unique())
+
+data["winddirection"].fillna(data["winddirection"].mode()[0], inplace=True)
+data["windspeed"].fillna(data["windspeed"].median(), inplace=True)
+
+# Check for any remaining missing values in the dataset after filling
+print(data.isnull().sum())
+
+# Convert the "rainfall" column to numerical values: "no" to 0 and "yes" to 1
+data["rainfall"] = data["rainfall"].map({"no": 0, "yes": 1})
+print(data.head())
+
+
+"""Exploratory Data Analysis"""
+sns.set_style("whitegrid")
+
+print(data.describe())
