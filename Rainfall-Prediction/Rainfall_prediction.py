bd5bf5c225dd64bd30af7fb09c009c2196551fc0
Williams Odunayo <williamsodunayo70@gmail.com>
2025-01-28 20:34:47 +0100

    Enhance data processing in Rainfall_prediction.py by loading dataset with correct path, stripping whitespace from column names, and checking for missing values.

--- Rainfall-Prediction/Rainfall_prediction.py
+++ Rainfall-Prediction/Rainfall_prediction.py
@@ -1,17 +1,24 @@
 # import libraries
 
 import numpy as np
+import pandas as pd
 
-
-
-# 
+# Load the dataset
 """Data Collection and Processing"""
-data = pd.read_csv('Rainfall.csv')
+data = pd.read_csv('./Rainfall.csv')
 print(data)
 
-# print the unique values in the "day" column
+# print the (unique) values in the "day" column
 print(data["day"].unique())
 
 # Drop the "day" column as it is not needed for the prediction
 data = data.drop(columns=["day"])
 print(data)
+
+print(data.info())
+
+# Remove leading or trailing whitespace from the column names
+data.columns = data.columns.str.strip()
+
+# Check for missing values in the dataset
+print(data.isnull().sum())
