76dea7578997b30ebde7619f6439ba4764c4d83d
Williams Odunayo <williamsodunayo70@gmail.com>
2025-01-29 08:55:08 +0100

    Enhance EDA script with improved dataset descriptions, correlation heatmap, boxplots for each column.

--- EDA_on_Rainfall_dataset.py
+++ EDA_on_Rainfall_dataset.py
@@ -3,17 +3,44 @@ from Data_processing_and_cleaning import data
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-print(data)
 
 # Quick description of the dataset
-print("Quick description", data.describe())
+print("Quick description:\n", data.describe())
 
 # Print the columns in the dataset
-print("Columns in dataset:", data.columns)
+print("Columns in dataset:\n", data.columns)
 
+
+# plot correlation heatmap of the dataset
 plt.figure(figsize=(10,8))
 sns.heatmap(data.corr(), annot=True, cmap="coolwarm", fmt=".2f")
 plt.title("Correlation Heatmap of Rainfall Dataset")
 plt.show()
 plt.close()
+
+
+# Set the style for seaborn plots
+sns.set_style("whitegrid")
+
+
+# plot boxplots for each column in the dataset
+plt.figure(figsize=(18,10))
+for i, column in enumerate(data.columns, 1):
+    
+    # Create a subplot for each column
+    plt.subplot(4, 3, i)
+    sns.boxplot(data[column])
+    plt.title(f"Boxplot of {column}")
+    
+plt.tight_layout()
+plt.show()
+
+
+
+# Plot the distribution of the "temperature" column
+sns.histplot(data["temparature"], kde=True, color="skyblue")
+plt.title("Distribution of Temperature")
+plt.xlabel("Temperature")
+plt.ylabel("Count")
+plt.show()
 
