52e3ce78bb361614e8036025397fcdc062d6a5d3
Williams Odunayo <williamsodunayo70@gmail.com>
2025-01-29 14:30:22 +0100

    Fix temperature typo in dataset and enhance Random Forest Classifier model with evaluation metrics and prediction on unknown data

--- RandomForestClassifier_Model.py
+++ RandomForestClassifier_Model.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from EDA_on_Rainfall_dataset import data_downsampled
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV, cross_val_score
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 
 
@@ -19,7 +20,7 @@ RFC_model = RandomForestClassifier(random_state=42)
 # Define the parameter grid for RandomForestClassifier
 param_grid_rf = {
     "n_estimators": [50, 100, 200],
-    "max_features": ["auto", "sqrt", "log2"],
+    "max_features": ["sqrt", "log2"],
     "max_depth": [None, 10, 20, 30],
     "min_samples_split": [2, 5, 10],
     "min_samples_leaf": [1, 2, 4]
@@ -30,12 +31,36 @@ GridSearchCV_RFC = GridSearchCV(estimator=RFC_model, param_grid=param_grid_rf, c
 GridSearchCV_RFC.fit(X_train, y_train)
 
 # output the best model from GridSearch
-best_RFC_model = GridSearchCV_RFC.best_estimator_
+Best_RFC_model = GridSearchCV_RFC.best_estimator_
 print("Best parameters for Random Forest: ", GridSearchCV_RFC.best_params_)
 
+"""Model Evaluation"""
 
-cv_scores = cross_val_score(best_RFC_model, X_train, y_train, cv = 5)
-print("Cross-validation scores for Random Forest: ", cv_scores)
-print("Mean cross-validation score for Random Forest: ", cv_scores.mean())
-print("Mean cross-validation score for Random Forest: ", np.mean(cv_scores))
+# Perform cross-validation on the training set
+cv_scores = cross_val_score(Best_RFC_model, X_train, y_train, cv=5)
+print("Cross-validation scores for Random Forest Classifier: ", cv_scores)
+print("Mean cross-validation score for Random Forest Classifier: ", cv_scores.mean())
 
+# Prediction the test set
+y_pred = Best_RFC_model.predict(X_test)
+
+# evaluate model's accuracy on the test set
+print("Test set Accuracy: ", accuracy_score(y_test, y_pred))
+print("Test set Confusion Matrix: \n", confusion_matrix(y_test, y_pred))
+print("Test set Classification Report: \n", classification_report(y_test, y_pred))
+
+
+"""Prediction on Unknown data"""
+# Prediction on unknown data
+unknown_data = (1021.8, 10.4, 78, 88, 20, 50.0, 28.8)
+
+# Reshape the data to match the model's expected input
+unknown_data = np.array(unknown_data).reshape(1, -1)
+
+# Make a prediction using the best Random Forest model
+prediction = Best_RFC_model.predict(unknown_data)
+
+if prediction == 0:
+    print("Prediction result: ", "No Rainfall")
+else:
+    print("Prediction result: ", "Rainfall")
