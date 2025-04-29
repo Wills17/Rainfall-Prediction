5e92cf436e9db9f5d5b10b91bb1b8403d394bc64
Williams Odunayo <williamsodunayo70@gmail.com>
2025-04-29 16:49:36 +0100

    Update Application.py to run the Flask app on host

--- Application.py
+++ Application.py
@@ -53,5 +53,5 @@ def predict():
     else:
         return render_template('predict.html')
             
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
